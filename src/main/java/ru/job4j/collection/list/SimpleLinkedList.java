015c4a5fc64ed3047498b9c751dd4c26ff54c1e4
starfirst57@gmail.com <GmI514TwerK>
2021-09-29 13:02:39 +0700

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,74 @@
+package ru.job4j.collection.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
+    private int modCount;
+
+    @Override
+    public void add(E value) {
+        Node<E> rsl = last;
+        last = new Node<>(rsl, value, null);
+        if (first == null) {
+            first = last;
+        }
+        if (rsl != null) {
+            rsl.next = last;
+        }
+        size++;
+        modCount++;
+        }
+
+    @Override
+    public E get(int index) {
+        Node<E> rsl = first;
+        for (int i = 0; i < Objects.checkIndex(index, size); i++) {
+            rsl = rsl.next;
+        }
+        return rsl.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            int expectedModCount = modCount;
+            Node<E> rsl = first;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return rsl != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> el = rsl;
+                rsl = rsl.next;
+                return el.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
