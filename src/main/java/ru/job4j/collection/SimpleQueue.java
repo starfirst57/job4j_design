2bb95455f5b405f04d782f504d85e8f6aea58b3c
starfirst57@gmail.com <GmI514TwerK>
2021-10-01 12:23:42 +0700

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        if (out.getSize() == 0) {
+            while (in.getSize() != 0) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,12 +2,19 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size;
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
+        size++;
         linked.addFirst(value);
+    }
+
+    public int getSize() {
+        return size;
     }
 }
