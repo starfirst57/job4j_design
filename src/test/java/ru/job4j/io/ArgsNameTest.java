5ba67e3e769b8ef8b17da163337bfacbd2cbbcf1
starfirst <starfirst57@gmail.com>
2021-10-31 11:08:48 +0700

    5.1. Именованные аргументы [#295518]

---
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class ArgsNameTest {
+
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertEquals(jvm.get("Xmx"), "512");
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertEquals(jvm.get("Xmx"), "512");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {});
+        jvm.get("Xmx");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongSomeArgument() {
+        ArgsName jvm = ArgsName.of(new String[] {"-enconding=UTF-8", "-Xmx="});
+    }
+}
