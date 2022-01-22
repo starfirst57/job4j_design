de63330c9c35293df6d05f699b7df49e0195cd37
starfirst <starfirst57@gmail.com>
2022-01-22 10:55:08 +0700

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/Apartment.java
@@ -0,0 +1,36 @@
+package ru.job4j.serialization;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Apartment implements Serializable {
+    private final String address;
+    private final int flat;
+    private final float square;
+    private final boolean hasBalcony;
+    private final Contact ownerContact;
+    private final String[] photos;
+
+    public Apartment (String address, int flat, float square,
+                      boolean hasBalcony, Contact ownerContact, String[] photos) {
+        this.address = address;
+        this.flat = flat;
+        this.square = square;
+        this.hasBalcony = hasBalcony;
+        this.ownerContact = ownerContact;
+        this.photos = photos;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{"
+                + "address='" + address + '\''
+                + ", flat=" + flat
+                + ", square=" + square
+                + ", hasBalcony=" + hasBalcony
+                + ", ownerContact=" + ownerContact
+                + ", photos=" + Arrays.toString(photos) +
+                '}';
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/Main.java
@@ -0,0 +1,29 @@
+package ru.job4j.serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        final Apartment apartment = new Apartment("Moscow, Pobedy st., 8, 12", 5,
+                120.5f, true,
+                new Contact(1234, "25-45-45"), new String[]{"facade.jpg","room1.jpg","room2.jpg"});
+
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(apartment));
+
+        String apartJson = "{\"address\":\"Moscow, "
+                + "Pobedy st., 8, 12\","
+                + "\"flat\":5,\"square\":120.5,"
+                + "\"hasBalcony\":true,"
+                + "\"ownerContact\":{"
+                + "\"zipCode\":1234,"
+                + "\"phone\":\"25-45-45\"},"
+                + "\"photos\":["
+                + "\"facade.jpg\","
+                + "\"room1.jpg\","
+                + "\"room2.jpg\"]}";
+
+        System.out.println(gson.fromJson(apartJson, Apartment.class));
+    }
+}
